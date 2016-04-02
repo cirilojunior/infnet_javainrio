e574e51889b84e617ec8ffb499e6fc74da5127f7
cirilojunior <cirilolxjunior@gmail.com>
2016-04-02 20:52:49 -0300

    phase listener para debug

---
+++ src/br/edu/infnet/javainrio/infrastructure/comum/JsfLifeCycleListener.java
@@ -0,0 +1,26 @@
+package br.edu.infnet.javainrio.infrastructure.comum;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class JsfLifeCycleListener implements PhaseListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		System.out.println("START PHASE " + event.getPhaseId());
+	}
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		System.out.println("END PHASE " + event.getPhaseId());
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+
+}
