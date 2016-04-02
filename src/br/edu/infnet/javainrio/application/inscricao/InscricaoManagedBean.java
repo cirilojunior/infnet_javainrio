b61fa8d9e903f5c20b0700a8f1337361a505ffcd
Pedro Paulo Vaccani dos Santos Neto <pedrovaccani@gmail.com>
2016-04-01 22:27:37 -0300

    Added files via upload

--- src/br/edu/infnet/javainrio/application/inscricao/InscricaoManagedBean.java
+++ src/br/edu/infnet/javainrio/application/inscricao/InscricaoManagedBean.java
@@ -1,12 +1,31 @@
 package br.edu.infnet.javainrio.application.inscricao;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
-@ManagedBean
+import br.edu.infnet.javainrio.infrastructure.inscricao.InscricaoRepositoryJpa;
+
+@ManagedBean(name = "inscricaoMB")
 public class InscricaoManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private List<InscricaoDTO> lista = new ArrayList<>();
+	
+	InscricaoRepositoryJpa jpa = new InscricaoRepositoryJpa();
 
+	public InscricaoManagedBean(){
+		buscarInscritos();
+	}
+	
+	public void buscarInscritos() {
+		lista = jpa.listar();
+	}
+	
+	public List<InscricaoDTO> getLista() {
+		return lista;
+	}
 }
