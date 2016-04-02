0b52a6ac1ee3daa2212f8ed64a0e584a8d3933cb
Pedro Paulo Vaccani dos Santos Neto <pedrovaccani@gmail.com>
2016-04-01 22:28:29 -0300

    Added files via upload

--- src/br/edu/infnet/javainrio/domain/inscricao/InscricaoRepository.java
+++ src/br/edu/infnet/javainrio/domain/inscricao/InscricaoRepository.java
@@ -1,7 +1,13 @@
 package br.edu.infnet.javainrio.domain.inscricao;
 
+import java.util.List;
+
+import br.edu.infnet.javainrio.application.inscricao.InscricaoDTO;
+
 public interface InscricaoRepository {
 	
 	void confirmar(Inscricao inscricao);
+	
+	List<InscricaoDTO> listar();
 
 }
